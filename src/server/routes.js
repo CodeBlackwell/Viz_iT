7350b65cd8da3bcf7428f743108cf09f805a666b
LeChristopher Blackwell <codeblackwell@gmail.com>
2018-04-05 13:54:45 -0600

    retrieve Tweets endpoint accepting screen_name parameter

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,50 +1,41 @@
 const router = require('koa-router')();
 const stats = require('../../build/stats.json');
-const Twitter = require('twitter-node-client').Twitter;
 const config = require('./config');
+const Twitter = require('twitter');
 
 const publicPath = stats.publicPath;
 
 let STYLE_URL;
 let SCRIPT_URL_APP = publicPath + [].concat(stats.assetsByChunkName.app)[0];
 if (process.env.NODE_ENV === 'production') {
-	STYLE_URL = (publicPath + [].concat(stats.assetsByChunkName.app)[1] + '?' + stats.hash);
-	SCRIPT_URL_APP += '?' + stats.hash;
+    STYLE_URL = (publicPath + [].concat(stats.assetsByChunkName.app)[1] + '?' + stats.hash);
+    SCRIPT_URL_APP += '?' + stats.hash;
 }
 
 // SCRIPT_URL_APP = '/public/app.min.js';
 // initiate twitter
-const twitter = new Twitter(config.Twitter);
+const client = new Twitter(config.Twitter);
 
 
 router.get('/client', async (ctx, next) => {
-	await ctx.render('index', {
-		version: stats.appVersion,
-		commit: stats.appCommit,
-		STYLE_URL: STYLE_URL,
-		SCRIPT_URL: SCRIPT_URL_APP
-	});
+    await ctx.render('index', {
+        version: stats.appVersion,
+        commit: stats.appCommit,
+        STYLE_URL: STYLE_URL,
+        SCRIPT_URL: SCRIPT_URL_APP
+    });
 });
 
 
-
-twitter.getUserTimeline({ screen_name: 'codeblackwell', count: '10'}, error, success);
-router.get('/bar', (ctx) => {
-
-    // ctx.body = 'hi';
-});
-
-// Twitter
-
-
-
-
-
-function error (err, res, body) {
-    console.log('ERROR [%s]', err);
-}
-function success(data) {
-    console.log('Data [%s]', data);
-}
+router.get(
+    '/tweets/:screen_name',
+    (ctx, next) => {
+        return client.get('statuses/user_timeline', ctx.params)
+            .then(tweets => {
+                ctx.body = tweets;
+                next();
+            });
+    }
+);
 
 module.exports = router;
