b02e5c23cc118c9b8eed6b9ebfa7e7afb1418879
LeChristopher Blackwell <codeblackwell@gmail.com>
2018-04-05 15:21:43 -0600

    react one-page-scroll

--- src/frontend/src/containers/Twitter/Twitter.js
+++ src/frontend/src/containers/Twitter/Twitter.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { KEY as TWITTER_KEY } from '../../constants/TwitterConstants'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { KEY as TWITTER_KEY } from '../../constants/TwitterConstants';
+import { ScrollContainer, ScrollSection } from 'react-onepage-scroll';
+
+import { Container } from '../../theme/grid';
 
 
 class Twitter extends Component {
     render () {
         const { tweets } = this.props;
-        console.log(tweets);
         return (
-            <div>
-                <h1>Twitter</h1>
-            </div>
+            <Container>
+                <ScrollContainer>
+                    <ScrollSection pageId={0} >
+                        <h3>Section 1</h3>
+                    </ScrollSection>
+                    <ScrollSection pageId={1} >
+                        <h3>Section 2</h3>
+                    </ScrollSection>
+                </ScrollContainer>
+            </Container>
         );
     }
 }
 
+
+
 const mapStateToProps = function (state) {
-    //@formatter:off
     const {
-        [TWITTER_KEY]: {
-            tweets
-        }
+        [TWITTER_KEY]: { tweets }
     } = state;
-    //@formatter:on
+
     return {
         tweets
     };
